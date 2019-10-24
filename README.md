ae26ea027c3118b853e94d872926b7d823699082
Huy Cuong <17520306@gm.uit.edu.vn>
2019-10-24 10:59:23 +0700

    migration database

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # do-an-web
+các lệnh tương tác DB
+composer create-tables : tự động tạo các bảng trong database
+composer create-tables-override: tự động tạo các bảng trong database, nếu bảng trong DB đã tồn tại thì sẽ ghi đè
--- doan/database/migrations/2014_10_12_000000_create_users_table.php
+++ doan/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('fullname')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('role');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
