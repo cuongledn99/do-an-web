17229806cd327da2045a386d1051ad789a8f4919
Banho <voduchau01011997@gmail.com>
2019-10-24 11:37:13 +0700

    chỉnh sửa lại asset của js cho hoàn thiện

--- doan/resources/views/layouts/adminpage.blade.php
+++ doan/resources/views/layouts/adminpage.blade.php
@@ -178,16 +178,16 @@
 
 
     <!-- jQuery  -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/detect.js"></script>
-    <script src="assets/js/fastclick.js"></script>
-    <script src="assets/js/jquery.blockUI.js"></script>
-    <script src="assets/js/waves.js"></script>
-    <script src="assets/js/jquery.nicescroll.js"></script>
-    <script src="assets/js/jquery.slimscroll.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/detect.js')}}"></script>
+    <script src="{{asset('assets/js/fastclick.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.blockUI.js')}}"></script>
+    <script src="{{asset('assets/js/waves.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.nicescroll.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.slimscroll.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.scrollTo.min.js')}}"></script>
     
 
 
@@ -196,10 +196,10 @@
 
 
     <!-- App js -->
-    <script src="assets/js/jquery.core.js"></script>
-    <script src="assets/js/jquery.app.js"></script>
-    <script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="{{asset('assets/js/jquery.core.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.app.js')}}"></script>
+    <script src="{{asset('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
     <script>
         $(document).ready(function () {
                         $('#tableStaffs').DataTable()
