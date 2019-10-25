e3980a466cec2dfb95419c7c9301eb5e0db62310
Banho <voduchau01011997@gmail.com>
2019-10-26 00:54:31 +0700

    button-delete-modal

--- doan/resources/views/adminpage/staffManagement.blade.php
+++ doan/resources/views/adminpage/staffManagement.blade.php
@@ -4,7 +4,7 @@
     <div class="col-12">
         <div class="card-box table-responsive">
             <h4 class="m-t-0 header-title">Staff Management</h4>
-            
+
 
             <table id="tableStaffs" class="table table-bordered">
                 <thead>
@@ -20,22 +20,47 @@
 
                 <tbody>
                     @foreach ($users as $user)
-                        
-                    
-                        
+
+
+
                     <tr>
                         <td>{{$user->id}}</td>
                         <td>{{$user->username}}</td>
                         <td>{{$user->role}}</td>
                         <td>{{$user->email}}</td>
                         <td>
-                               <button class="btn btn-icon waves-effect waves-light btn-danger m-b-5">
-                                    <i class="fa fa-trash"></i>
-                                </button>
-                                <button class="btn btn-icon waves-effect waves-light btn-success m-b-5">
-                                    <i class="fa fa-eye"></i>
-                                </button>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                            <i class="fa fa-trash"></i>
+                            </button>
+                            
+                            <button class="btn btn-icon waves-effect waves-light btn-success m-b-5">
+                                <i class="fa fa-eye"></i>
+                            </button>
                         </td>
+                        <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Are you sure to want delete ?</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        
+        
+        <!-- Modal footer -->
+        <div class="modal-footer" style="justify-content:center;">
+          <button type="button" class="btn btn-success" data-dismiss="modal">Yes</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+        </div>
+        
+         
+        
+        
+      </div>
+    </div>
+  </div>
                     </tr>
                     @endforeach
                 </tbody>
