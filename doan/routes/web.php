d0770eb0d59c1947da9c1c532f9628fc130ba29d
Huy Cuong <17520306@gm.uit.edu.vn>
2019-10-27 10:55:58 +0700

    giao dien homepage

--- doan/routes/web.php
+++ doan/routes/web.php
@@ -4,3 +4,8 @@ use App\Utils\UrlUtil;
 
 Route::get('/', 'Pages\\NonAuths\\HomeController@index');
 
+
+Route::get('/admin','AdminPageController@renderAdminPage');
+Route::get('/admin/manageStaff','AdminPageController@renderStaff');
+Route::get('admin/manageUser','AdminPageController@renderUser');
+Route::get('admin/manageProduct','AdminPageController@renderProduct');
